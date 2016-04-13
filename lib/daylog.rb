f7575c8fcf0ef7a194ba402180c9c0ec6faf31e3
saxsir <saxsir.256+github@gmail.com>
2016-04-13 13:56:41 +0900

    覚えてない変更があったのでコミット

--- lib/daylog.rb
+++ lib/daylog.rb
@@ -1,5 +1,33 @@
+require "net/http"
+require "uri"
+require 'yaml'
+require "json"
+
 module Daylog
-  def self.hello()
-    return "Hello, Daylog"
+
+
+  def self.open_yaml(fp)
+    str = open(fp, &:read)
+    return YAML.load(str)
+  end
+
+  def self.find_token(yaml)
+    return yaml.has_key?("token") ? yaml["token"] : ""
+  end
+
+  def self.auth?
+  fp = File.expand_path('../../config.yml', __FILE__)
+  config = self.open_yaml(fp)
+  token = self.find_token(config)
+
+    `curl -sS #{}`
+    res = self.get_json('https://slack.com/api/auth.test', {
+      "token" => token
+    })
+
+    p res
+    p res.body
+
+    return true if JSON.parse(res.body)["ok"] else false
   end
 end
--- test/daylog_test.rb
+++ test/daylog_test.rb
@@ -1,7 +1,31 @@
 require File.expand_path('../test_helper', __FILE__)
 
 class DaylogTest < Minitest::Test
-  def test_Daylogモジュールのメソッドが呼べる
-    assert_equal 'Hello, Daylog', Daylog.hello
+
+  describe "#open_yaml" do
+    it "ファイルが見つからなかったらエラー" do
+      fp = File.expand_path('../../foo.yml', __FILE__)
+      assert_raises do
+        Daylog.open_yaml(fp)
+      end
+    end
+
+    it "ファイルが見つかればYAMLファイルの中身をRubyのオブジェクトにして返す" do
+      fp = File.expand_path('../../config.sample.yml', __FILE__)
+      assert_equal({"token" => "your-token"}, Daylog.open_yaml(fp))
+    end
+  end
+
+  describe "#find_token" do
+    it "yamlファイルのtokenの値を返す" do
+      config = Daylog.open_yaml(File.expand_path('../../config.sample.yml', __FILE__))
+      assert_equal "your-token", Daylog.find_token(config)
+    end
+  end
+
+  describe "#auth?" do
+    it "slackのapiを利用できる（tokenで認証できる）" do
+      assert_equal true, Daylog.auth?
+    end
   end
 end
